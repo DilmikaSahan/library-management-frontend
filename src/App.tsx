e4fbeae3e5f90181f074d73621b9e0c1eb2233c3
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2026-02-16 21:12:08 +0530

    Add css style and update Home page for resposive designing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
+import './App.css';
 import Home from './pages/Home';
+import ViewBook from './pages/ViewBook';
+import CreateBook from './pages/CreateBook';
+import EditBook from './pages/EditBook';
 
 function App() {
-  return(
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+  return (
+    <div className="app-root">
+      <Router>
+        <header className="app-header">
+          <div className="app-header-inner">
+            <h1 className="app-title">Library Management</h1>
+          </div>
+        </header>
 
+        <main className="app-main">
+          <div className="app-main-inner">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/create" element={<CreateBook />} />
+              <Route path="/edit/:id" element={<EditBook />} />
+              <Route path="/book/:id" element={<ViewBook />} />
+            </Routes>
+          </div>
+        </main>
+      </Router>
+    </div>
   );
 }
 
-export default App
+export default App;
